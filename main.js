fc8285866722bac03a448a3ff2500e37a0e42ba8
MrHTD <mughis01@gmail.com>
2025-11-28 19:22:16 +0500

    added countdown!

--- main.js
+++ main.js
@@ -80,6 +80,29 @@ function init() {
     animate();
 }
 
+// 7. Countdown Timer
+
+const weddingDate = new Date("March 27, 2026 13:00:00").getTime();
+
+const x = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    const countdownElement = document.getElementById("countdown");
+
+    if (distance < 0) {
+        clearInterval(x);
+        countdownElement.innerHTML = "We are married!";
+    } else {
+        countdownElement.innerHTML = `Remaining: ${days}d ${hours}h ${seconds}s`;
+    }
+}, 1000);
+
 window.addEventListener('resize', resize);
 
 // 6. Master Window Load
